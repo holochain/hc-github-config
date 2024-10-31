f05252f7164a6f131f8e91ed12ea0e110ba04824
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-10-31 19:09:18 +0000

    chore: fix warning when using pulumi with go plugin, need to be in same directory

--- flake.nix
+++ flake.nix
@@ -5,13 +5,28 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=24.05";
   };
 
-  outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
-        devShells.x86_64-linux.default = pkgs.mkShell {
-          packages = with pkgs; [
-            pulumi
-            pulumiPackages.pulumi-language-go
-            go
-          ];
-        };
-  };
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pulumiBundle = pkgs.stdenv.mkDerivation {
+        name = "pulumi-bundle";
+        phases = [ "installPhase" "fixupPhase" ];
+        buildInputs = with pkgs; [ pulumi pulumiPackages.pulumi-language-go ];
+        installPhase = ''
+          mkdir -p $out/bin
+          mkdir -p $out/share
+          cp ${pkgs.pulumi}/bin/* $out/bin/
+          cp -r ${pkgs.pulumi}/share $out/share
+          cp ${pkgs.pulumiPackages.pulumi-language-go}/bin/* $out/bin/
+        '';
+      };
+    in
+    {
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+        ] ++ [ pulumiBundle ];
+      };
+    };
 }
