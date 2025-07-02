cc1ee8fe0c1fee1e6ab871ba87db21fb1d14c8c5
Paul d'Aoust <paul.daoust@holo.host>
2025-07-02 14:45:12 -0700

    fix: line up hc-static-site args with actual config

--- main.go
+++ main.go
@@ -527,9 +527,21 @@ func main() {
 		// hc-static-site
 		//
 		hcStaticSiteRepositoryArgs := StandardRepositoryArgs("hc-static-site", nil)
-		hcStaticSiteRepositoryArgs.Description = pulumi.String("The hosted static site builder for the holochain.org website")
-		hcStaticSiteRepositoryArgs.HasDiscussions = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.Description = pulumi.String("Static website")
+		hcStaticSiteRepositoryArgs.AllowAutoMerge = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.AllowRebaseMerge = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.AllowSquashMerge = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.AllowUpdateBranch = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.AutoInit = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.DeleteBranchOnMerge = pulumi.Bool(false)
+		hcStaticSiteRepositoryArgs.HasDownloads = pulumi.Bool(true)
+		hcStaticSiteRepositoryArgs.visibility = pulumi.String("private")
 		hcStaticSiteRepositoryArgs.HomepageUrl = pulumi.String("https://www.holochain.org")
+		hcStaticSiteRepositoryArgs.MergeCommitMessage = pulumi.String("PR_TITLE")
+		hcStaticSiteRepositoryArgs.MergeCommitTitle = pulumi.String("MERGE_MESSAGE")
+		hcStaticSiteRepositoryArgs.SquashMergeCommitMessage = pulumi.String("COMMIT_MESSAGES")
+		hcStaticSiteRepositoryArgs.MergeCommitTitle = pulumi.String("COMMIT_OR_PR_TITLE")
+		hcStaticSiteRepositoryArgs.VulnerabilityAlerts = pulumi.Bool(false)
 		hcStaticSite, err := github.NewRepository(ctx, "hc-static-site", &hcStaticSiteRepositoryArgs, pulumi.Import(pulumi.ID("hc-static-site")))
 		if err != nil {
 			return err
